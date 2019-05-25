7b813dac44ba16349b811c067666c6cd837f9781
yindaheng98 <yindaheng98@163.com>
2019-05-25 20:31:53 +0800

    i

--- Data/node/app/controllers/setup.js
+++ Data/node/app/controllers/setup.js
@@ -4,13 +4,14 @@ const con = require('../controllers/connections');
 function InitData() {
     con.redis.flushall(() => {
         let data_str = fs.readFileSync(con.file);
-        let data=JSON.parse('{'+data_str.slice(0,-1)+'}');
+        if (data_str.length <= 0) return;
+        let data = JSON.parse('{' + data_str.slice(0, -1) + '}');
         let runtime;
-        for(let k in data){
-            con.redis.set(parseInt(k),data[k]);
-            runtime=[k,data[k]];
+        for (let k in data) {
+            con.redis.set(parseInt(k), data[k]);
+            runtime = [k, data[k]];
         }
-        con.runtime.put(parseInt(runtime[0]),runtime[1]);
+        con.runtime.put(parseInt(runtime[0]), runtime[1]);
         console.log('数据初始化');
         console.log(data);
     });
