c929cb8e8b773b458e2992315628c81093db27e8
yindaheng98 <yindaheng98@163.com>
2019-05-27 01:10:20 +0800

    i

--- Data/node/app/controllers/setup.js
+++ Data/node/app/controllers/setup.js
@@ -3,9 +3,19 @@ const con = require('../controllers/connections');
 
 function InitData() {
     con.redis.flushall(() => {
-        let data_str = fs.readFileSync(con.file);
-        if (data_str.length <= 0) return;
-        let data = JSON.parse('{' + data_str.slice(0, -1) + '}');
+        let data = {};
+        try {
+            let data_str = fs.readFileSync(con.file);
+            if (data_str.length <= 0) return;
+            data = JSON.parse('{' + data_str.slice(0, -1) + '}');
+        } catch (e) {
+            console.log('数据库有错误');
+            console.log(e);
+            fs.writeFile(con.file, '', 'utf8', function (error) {
+                if (error) return console.log(error);
+                console.log('数据库已重置');
+            });
+        }
         let runtime;
         for (let k in data) {
             con.redis.set(parseInt(k), data[k]);
