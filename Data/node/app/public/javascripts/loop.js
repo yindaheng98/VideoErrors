d84da36d54caae2390f485cd08f37cf83efc7db2
yindaheng98 <yindaheng98@163.com>
2019-05-27 18:50:39 +0800

    initial commit

--- Data/node/app/public/javascripts/loop.js
+++ Data/node/app/public/javascripts/loop.js
@@ -1,4 +1,7 @@
+let b=false;
 function roll_updatePage() {
+    if(b)return;
+    b=true;
     var ajaxTimeoutTest = $.ajax({
         url: "runtime",
         timeout: 40000, //超时时间设置，单位毫秒
@@ -7,12 +10,14 @@ function roll_updatePage() {
         dataType: 'json', //返回的数据格式
         success: function (data) {
             updatePage(data);
+            b=false;
             roll_updatePage();
         },
         complete: function (XMLHttpRequest, status) { //求完成后最终执行参数
             // 设置timeout的时间，通过检测complete时status的值判断请求是否超时，如果超时执行响应的操作
             console.log(status);//超时,status还有success,error等值的情况
             ajaxTimeoutTest.abort();
+            b=false;
             roll_updatePage();
         }
     });
